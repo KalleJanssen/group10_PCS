264830f0cd0ba53a9674fc912cd3a8fcf2ee9c29
Kalle Janssen <Kallejanssen@me.com>
2020-01-21 12:51:38 +0100

    better

--- earth_sim.py
+++ earth_sim.py
@@ -12,7 +12,7 @@ def hourly_it(start, finish):
     Returns a list of all timepoints between a start and end date
     """
     while finish > start:
-        start = start + timedelta(seconds=60)
+        start = start + timedelta(seconds=1)
         yield start
 
 
@@ -40,10 +40,15 @@ start = datetime(2020, 1, 10, 14, 0, 0)
 finish = datetime(2020, 1, 10, 16, 0, 0)
 filename = open("data/output.txt", "r").read().splitlines()
 scene.autoscale = False
+plot1 = 0
+total = 0
+list_line = []
 for i in hourly_it(start, finish):
     #scene.waitfor('click')
     index = 0
-    rate(500)
+    plot1 += 1
+    rate(3005)
+
     # Required to make animation visible / refresh smoothly (keeps program from running faster
     #    than 1000 frames/s)
     for line in filename:
@@ -53,19 +58,25 @@ for i in hourly_it(start, finish):
         if line[0] == '2':
             index += 1
             l2 = line
+
         lati, longi = lati_longi(l1, l2, i, 5)
+
         if (lati, longi) == (80, -15):
-            e["trial{}" .format(index)] = curve(color = color.red, radius = 5e3)
-            d["satellite{}" .format(index)] = sphere(radius = 5e4, color = color.red)
+            if line not in list_line:
+                e["trial{}" .format(index)] = curve(color = color.red, radius = 5e3)
+                d["satellite{}" .format(index)] = sphere(radius = 5e4, color = color.red)
+                list_line.append(line)
+
         satellite1 = twoline2rv(l1, l2, wgs72)
         time_list = (int(str(i)[0:4]), int(str(i)[5:7]), int(str(i)[8:10]),
             int(str(i)[11:13]), int(str(i)[14:16]), int(str(i)[17:19]))
         xyz, velocity = satellite1.propagate(time_list[0], time_list[1],
             time_list[2], time_list[3], time_list[4], time_list[5])
+
         satellite = d["satellite{}" .format(index)]
         satellite.pos = vector(xyz[1] * 1000, xyz[2] * 1000, xyz[0] * 1000)
         trial = e["trial{}" .format(index)]
         trial.append(pos = satellite.pos)
 
-        if index == 1000:
+        if index == 3003:
             break
